1f573ea9d5961d270aee68432cb1e97ddfcb9c94
Amish Suchak <amish.suchak@gmail.com>
2023-07-08 21:13:39 -0500

    changed from streamlit to flask

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-import streamlit as st
+from flask import Flask, request, render_template
 import requests
 
-user_input = st.text_input("Enter your text here")
-submit = st.button('Submit')
+app = Flask(__name__)
 
-if submit:
-    url = 'http://sentimentreview.eastus.azurecontainer.io:8001/predict_probability'
-    response = requests.post(url, json={"review": user_input})
-    prediction = response.json()
-    st.write(f'The prediction is: {prediction}')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        user_input = request.form['text']
+        url = 'http://sentimentreview.eastus.azurecontainer.io:8001/predict_probability'
+        response = requests.post(url, json={"review": user_input})
+        prediction = response.json()
 
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
